39b5326a571319793ecedf1cc9724b7bd82d779a
S-Kovalenko <s.kovalenko.dv@gmail.com>
2016-11-30 21:23:27 +0200

    Add styles to header

--- themes/HaiConference/templates/header.php
+++ themes/HaiConference/templates/header.php
@@ -1,5 +1,5 @@
 <?php
-$imgLogo =  get_template_directory_uri().'/dist/images/logo.png'; ;
+$imgLogo =  get_template_directory_uri().'/assets/images/logo.png'; ;
 
 ?>
 
@@ -7,26 +7,29 @@ $imgLogo =  get_template_directory_uri().'/dist/images/logo.png'; ;
 
 <div class="uk-container uk-container-center uk-margin-bottom">
   <div class="uk-grid">
-    <div class="uk-width-small-1-1 uk-width-medium-4-10 uk-width-large-3-10">
-      <img src="<?php echo $imgLogo; ?>" alt="Logo" class="uk-img-preserve">
+    <div class="uk-width-small-1-1 uk-width-medium-3-10 uk-width-large-2-10">
+      <img src="<?php echo $imgLogo; ?>" alt="Logo" class="uk-img-preserve logo">
     </div>
-    <div class="uk-width-small-1-1 uk-width-medium-6-10 uk-width-large-7-10">
-        Что такое Lorem Ipsum?
+    <div class="header-info uk-width-small-1-10 uk-width-medium-7-10 uk-width-large-8-10">
+        <div class="uk-width-small-1-1 uk-width-medium-1-1 uk-width-large-1-1 uk-text-right">
+          <span>Пленарное заседание состоялось 20.04.2016 в 14:00 в 307гк НАКУ ХАИ</span>
+        </div>
+        <div class="uk-width-small-1-1 uk-width-medium-1-1 uk-width-large-1-1 uk-text-right">
+          <span>Свяжитесь с нами: (057) 212342342</span>
+        </div>
+          <?php
+          if (has_nav_menu('primary_navigation')) :
+              wp_nav_menu( array(
+                'theme_location'    => 'primary_navigation',
+                'depth'             => 2,
+                'container'         => '',
+                'menu_class'        => 'uk-navbar-nav uk-hidden-small',
+                'fallback_cb'       => 'basey_primary_menu::fallback',
+                'walker'            => new Nav_primary_menu())
+          );
+          endif;
+          ?>
     </div>
   </div>
  </div>
-<div class="uk-container uk-container-center">
-      <?php
-      if (has_nav_menu('primary_navigation')) :
-          wp_nav_menu( array(
-            'theme_location'    => 'primary_navigation',
-            'depth'             => 2,
-            'container'         => '',
-            'menu_class'        => 'uk-navbar-nav uk-hidden-small',
-            'fallback_cb'       => 'basey_primary_menu::fallback',
-            'walker'            => new Nav_primary_menu())
-			);
-      endif;
-      ?>
-</div>
 </header>
