1dc56d243e8ba56d16c089f3aca67d852a790989
S-Kovalenko <s.kovalenko.dv@gmail.com>
2016-12-03 17:45:27 +0200

    Add changes to bottom blocks and header

--- themes/HaiConference/templates/bottom-section-content.php
+++ themes/HaiConference/templates/bottom-section-content.php
@@ -1,5 +1,5 @@
 <div class="sections uk-grid uk-text-right uk-margin-bottom">
-    <div class="section uk-width-1-4">
+    <div class="section uk-width-small-1-1 uk-width-medium-1-4 uk-width-large-1-4">
         <a href="#">
             <div class="section-header">
                 <span class="uk-width-1-2">Цель и тематика конференции</span>
@@ -9,7 +9,7 @@
             <span>Общение ученых высшей школы и представителей производства для ознакомления с новейшими мировыми и научно-техническими достижениями в области создания и обеспечения жизненного цикла авиационной техники</span>
         </div>
     </div>
-    <div class="section uk-width-1-4">
+    <div class="section uk-width-small-1-1 uk-width-medium-1-4 uk-width-large-1-4">
         <a href="#">
             <div class="section-header">
                 <span>Секции и материалы конференции</span>
@@ -19,7 +19,7 @@
             <span>Общение ученых высшей школы и представителей производства для ознакомления с новейшими мировыми и научно-техническими достижениями в области создания и обеспечения жизненного цикла авиационной техники</span>
         </div>
     </div>
-    <div class="section uk-width-1-4">
+    <div class="section uk-width-small-1-1 uk-width-medium-1-4 uk-width-large-1-4">
         <a href="#">
             <div class="section-header">
                 <span>Правила оформления тезисов</span>
@@ -29,7 +29,7 @@
             <span>Общение ученых высшей школы и представителей производства для ознакомления с новейшими мировыми и научно-техническими достижениями в области создания и обеспечения жизненного цикла авиационной техники</span>
         </div>
     </div>
-    <div class="section uk-width-1-4">
+    <div class="section uk-width-small-1-1 uk-width-medium-1-4 uk-width-large-1-4">
         <a href="#">
             <div class="section-header">
                 <span>Необходимые материалы</span>
--- themes/HaiConference/templates/header.php
+++ themes/HaiConference/templates/header.php
@@ -7,30 +7,34 @@ $imgLogo =  get_template_directory_uri().'/assets/images/logo.png';
 
 <div class="uk-container uk-container-center uk-margin-top uk-margin-bottom">
   <div class="uk-grid">
-    <div class="uk-width-small-1-1 uk-width-medium-3-10 uk-width-large-2-10">
+    <div class="uk-width-small-1-2 uk-width-medium-3-10 uk-width-large-2-10">
       <a href="<?php echo get_home_url(); ?>">
         <img src="<?php echo $imgLogo; ?>" alt="Logo" class="uk-img-preserve logo">
       </a>
     </div>
-    <div class="header-info uk-width-small-1-1 uk-width-medium-7-10 uk-width-large-8-10">
+    <div class="header-info uk-width-small-1-2 uk-width-medium-7-10 uk-width-large-8-10">
         <div class="meeting-date uk-width-small-1-1 uk-width-medium-5-6 uk-width-large-5-6 uk-text-right uk-push-1-10">
           <span>Пленарное заседание состоялось 20.04.2016 в 14:00 в 307гк НАКУ ХАИ</span>
         </div>
         <div class="contact-phone uk-width-small-1-1 uk-width-medium-5-6 uk-width-large-5-6 uk-text-right uk-push-1-10">
           <span>Свяжитесь с нами: (057) 212342342</span>
         </div>
-          <?php
-          if (has_nav_menu('primary_navigation')) :
-              wp_nav_menu( array(
-                'theme_location'    => 'primary_navigation',
-                'depth'             => 2,
-                'container'         => '',
-                'menu_class'        => 'uk-navbar-nav uk-hidden-small',
-                'fallback_cb'       => 'basey_primary_menu::fallback',
-                'walker'            => new Nav_primary_menu())
-          );
-          endif;
-          ?>
+        <div class="uk-container">
+          <div class="uk-grid">
+            <?php
+            if (has_nav_menu('primary_navigation')) :
+                wp_nav_menu( array(
+                  'theme_location'    => 'primary_navigation',
+                  'depth'             => 2,
+                  'container'         => '',
+                  'menu_class'        => 'uk-navbar-nav uk-hidden-small',
+                  'fallback_cb'       => 'basey_primary_menu::fallback',
+                  'walker'            => new Nav_primary_menu())
+            );
+            endif;
+            ?>
+            </div>
+          </div>
     </div>
   </div>
  </div>
